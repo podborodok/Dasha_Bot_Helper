94c3737f0b9f2c79091f726b0ff41e29708e78ba
podborodok <katya.mih22@gmail.com>
2024-12-07 19:10:43 +0300

    delete_user_from_chat added

--- data_base.py
+++ data_base.py
@@ -13,10 +13,12 @@ chat_user = Table(
     extend_existing=True,
 )
 
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
+
 
 class Chat(Base):
     __tablename__ = 'chat'
@@ -34,12 +36,21 @@ class Chat(Base):
     @valid_users.setter
     def valid_users(self, value):
         self.valid_users_str = ','.join(value)
+
     def add_user(self, user, valid=False):
         self.users.append(user)
         if valid:
-            self.valid_users = self.valid_users + [user.name]
+            self.valid_users.append(user.name)
+
+    def delete_user_from_chat(self, user_id, session):
+        user = session.query(User).filter_by(id=user_id).first()
+        if user:
+            self.users.remove(user)
+            session.execute(chat_user.delete().where(chat_user.c.chat_id == self.id).where(chat_user.c.user_id == user.id))
+        session.commit()
+
     def add_valid_user(self, username):
-        self.valid_users = self.valid_users + [username]
+        self.valid_users.append(username)
 
     def delete_from_valid_users(self, username):
         if username in self.valid_users:
@@ -48,8 +59,8 @@ class Chat(Base):
     def commit_users(self, session):
         for user in self.users:
             is_valid = user.name in self.valid_users
-            existing_entry = session.query(chat_user).filter_by(chat_id=self.id, user_id=user.id).first()
-            if not existing_entry:
+            find_chat_user = session.query(chat_user).filter_by(chat_id=self.id, user_id=user.id).first()
+            if not find_chat_user:
                 session.execute(chat_user.insert().values(chat_id=self.id, user_id=user.id, valid=is_valid))
             else:
                 session.execute(chat_user.update().where(chat_user.c.chat_id == self.id).where(
